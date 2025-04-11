1bf49583f48a7da81904cdf8e57c28a06cdc82d8
hoangdk <hoangdk@viettelpost.com.vn>
2025-04-11 11:36:27 +0700

    init project toiec web

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ["geist"],
+  experimental: {
+    optimizePackageImports: ["@chakra-ui/react"],
+  },
 };
 
 export default nextConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import "@/styles/globals.css";
+import {ChakraProvider, defaultSystem} from "@chakra-ui/react"
+import { GeistSans } from "geist/font/sans";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function App({Component, pageProps}) {
+  return (
+    <main className={GeistSans.className}>
+      <ChakraProvider value={defaultSystem}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </main>
+  )
 }
